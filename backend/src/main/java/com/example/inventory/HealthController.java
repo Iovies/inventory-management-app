6fb5c98b26cbc0a17aad2547ae40abb043cc9dff
Iulik <westovies@gmail.com>
2026-06-02 23:35:04 +0300

    Test automatic CI CD deployment

--- backend/src/main/java/com/example/inventory/HealthController.java
+++ backend/src/main/java/com/example/inventory/HealthController.java
@@ -18,6 +18,6 @@ public class HealthController {
     
     @GetMapping("/test")
     public String test() {
-        return "Test lab 4";
+        return " Laboratorul 4 a fost realizat cu succes";
     }
 }
